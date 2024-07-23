aa467369bf5ae26ac36698b0d1b4339965435a6a
Molean <cnmolean@gmail.com>
2024-07-23 20:07:36 +0800

    update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,6 @@ group = "com.molean"
 version = "1.0"
 
 dependencies {
-    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.7.1")
-    implementation("io.github.goooler.shadow:shadow-gradle-plugin:8.1.7")
     implementation("net.fabricmc:access-widener:2.1.0")
     implementation("net.fabricmc:tiny-remapper:0.10.1:fat")
     implementation("com.google.code.gson:gson:2.10.1")
@@ -24,7 +22,7 @@ java {
 gradlePlugin {
     plugins {
         create("ignite") {
-            id = "com.molean.ignite"
+            id = "com.molean.ignite.access-widener"
             implementationClass = "com.molean.IgniteAWP"
         }
     }
